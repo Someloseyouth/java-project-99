358dd6302217f53482019e2c61cf053f1db29dba
someloseyouth <valeriknikulin@gmail.com>
2026-06-06 12:19:51 +0300

    Add SonarCloud, GitHub Actions and badges

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ colorTo: green
 sdk: docker
 pinned: false
 ---
+
+# Java Project 99
+
+[![Actions Status](https://github.com/Someloseyouth/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Someloseyouth/java-project-99/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Someloseyouth_java-project-99&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Someloseyouth_java-project-99)
+
+## Ссылка на приложение
+
+https://someloseyouth-java-project-99.hf.space/welcome
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,31 @@
 plugins {
-	java
-	id("org.springframework.boot") version "4.0.6"
-	id("io.spring.dependency-management") version "1.1.7"
+java
+id("org.springframework.boot") version "4.0.6"
+id("io.spring.dependency-management") version "1.1.7"
+id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+toolchain {
+languageVersion = JavaLanguageVersion.of(21)
+}
 }
 
 repositories {
-	mavenCentral()
+mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+implementation("org.springframework.boot:spring-boot-starter")
+implementation("org.springframework.boot:spring-boot-starter-webmvc")
+implementation("org.springframework.boot:spring-boot-devtools")
+testImplementation("org.springframework.boot:spring-boot-starter-test")
+testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+useJUnitPlatform()
 }
