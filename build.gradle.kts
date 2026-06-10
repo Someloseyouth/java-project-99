aa0e2804c183821b8729376f310c497b226f2b31
Someloseyouth <valeriknikulin@email.com>
2026-06-11 00:23:45 +0300

    Fix sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.3.1.8318"
 }
 
 group = "hexlet.code"
